dff29d2462c67d22e225ebbd3a1c22d4441c3ecd
LeftRight92 <Michael_Bohea@msn.com>
2016-04-17 03:24:36 +0100

    Day 1

---
+++ Assets/Scripts/CameraTurntable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTurntable : MonoBehaviour {
+	Vector3 lastMousePosition;
+	[SerializeField]
+	bool flipVertical = false;
+	[SerializeField]
+	float verticalScaling = 1f;
+	[SerializeField]
+	bool flipHorizontal = false;
+	[SerializeField]
+	float horizontalScaling = 1f;
+	[SerializeField]
+	float verticalAngleMin = 10f;
+	[SerializeField]
+	float verticalAngleMax = 80f;
+	// Use this for initialization
+	void Start () {
+		lastMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentMousePosition = Input.mousePosition;
+
+		if (Input.GetMouseButton(2)) {
+			Vector3 diff = (lastMousePosition - currentMousePosition);
+			transform.rotation = Quaternion.Euler(
+				Mathf.Clamp(
+					transform.rotation.eulerAngles.x + (diff.y * verticalScaling * (flipVertical ? -1 : 1)),
+					verticalAngleMin,
+					verticalAngleMax),
+				transform.rotation.eulerAngles.y + (diff.x * horizontalScaling * (flipHorizontal ? -1 : 1)),
+				0);
+		}
+
+		lastMousePosition = currentMousePosition;
+	}
+}
---
+++ Assets/SemiTransparent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SemiTransparent : MonoBehaviour {
+
+	[SerializeField]
+	[Range(0,1)]
+	float alpha = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
